ebb3c7e7f49702e491461c33ee3a0f1635ba16a3
riagersappe <72521850+riagersappe@users.noreply.github.com>
2021-04-14 18:34:09 -0700

    Add files via upload

---
+++ c30/bird.js
@@ -0,0 +1,16 @@
+class bird extends base {
+    constructor(x,y,w,h){
+        
+
+       super(x,y,w,h) 
+        this.image=loadImage("sprites/bird.png")
+
+        
+    }
+    display(){
+        //this.body.position.x = mouseX
+        //this.body.position.y = mouseY
+    
+super.display()
+    }
+  }
